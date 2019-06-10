afe213a1a3a98cbd24dcda79cbac55e84b016fb2
lars-m <lam@cphbusiness.dk>
2019-06-10 11:18:33 +0200

    minor

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,35 +1,17 @@
 exports.onServiceWorkerUpdateFound = o => {
   console.log("SW", JSON.parse(JSON.stringify(o)));
-  console.log("SW2", o);
-  // const answer = window.confirm(
-  //   `This application has been updated. ` +
-  //     `Reload to display the latest version?`
-  // )
-  // const answer = true;
-  // if (answer === true) {
-  //   window.location.reload(true);
-  // }
-  //Probably not the right place to do this, but here it is:
+  
   if ("Notification" in window) {
     if (Notification.permission !== "denied") {
       Notification.requestPermission().then(function(result) {
-        console.log("Attempted to get permission for Notificatioins", result);
+        console.log("Attempted to get permission for Notifications", result);
       });
     }
   }
 };
 exports.onServiceWorkerUpdateReady = o => {
   console.log("OnServiceWorkerUpdateFound", o);
-  //window.location.reload(true);
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  )
-
-  if (answer === true) {
-    window.location.reload()
-  }
-
+  window.location.reload(true);
   if ("Notification" in window) {
     var notification = new Notification("App was updated", {
       body: "Your app has new content"
